39be6404c587ed67657d421c0eee5dfac2a96423
Eric <x16.tux@gmail.com>
2017-02-09 22:51:35 -0500

    added step

--- scripts/dailyLoad.sh
+++ scripts/dailyLoad.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 echo "Populating data files ====="
 ./pollHistoricalData.sh
+echo "Running the Hadoop Processor Job ====="
+yarn jar /home/worker/HadoopProcessor-0.1.0.jar com.eulicny.driver.HistoricalLoadDriver
 echo "Populating Tables   ====="
 ./populateTable.sh
