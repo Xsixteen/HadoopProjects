7eb499430e96fb37155085e05b12f318a5f9c357
Eric <x16.tux@gmail.com>
2017-02-12 14:12:18 -0500

    HDFS Driver ADded

---
+++ src/main/java/com/eulicny/common/HiveDB.java
@@ -0,0 +1,24 @@
+package com.eulicny.common;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class HiveDB {
+	
+	 public static final String HIVE_JDBC_DRIVER = "org.apache.hadoop.hive.jdbc.HiveDriver";
+	 public static final String HIVE_JDBC_EMBEDDED_CONNECTION = "jdbc:hive://";
+	 private static String driverName = "org.apache.hadoop.hive.jdbc.HiveDriver";
+
+	 public Statement getConnection() throws ClassNotFoundException,
+	            SQLException {
+	        Class.forName(HIVE_JDBC_DRIVER);
+	        Connection connection = DriverManager.getConnection(
+	                HIVE_JDBC_EMBEDDED_CONNECTION, "", "");
+
+	        Statement statement = connection.createStatement();
+	        return statement;
+	 }
+
+}
