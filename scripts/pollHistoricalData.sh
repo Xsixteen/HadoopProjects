bad5687b2cc5dc561e3459b8a02995e0dbf3a32b
Eric <x16.tux@gmail.com>
2017-02-12 16:06:01 -0500

    fixes for wget

--- scripts/pollHistoricalData.sh
+++ scripts/pollHistoricalData.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 symbols=(SPY F CSCO T QQQ XLE XLV GM BA O AAPL GOOGL CAT XOM MSFT GE HD SI IBM AXP MMM SNE DIS MET ACL ALL HON BBY TXN AMZN LU MGM LLL ET HSY KO NVDA PIXR CTXS WSM CRM TSN LLY FFIV MTD PNRA TTC DPZ NNI MCD WAG GSK PG FE TM COP VZ DCM GS ORCL MRK)
 
-"variables.sh"
+ROOT_LOCATION="$(./variables.sh)"
+
+echo "Base Location = ${ROOT_LOCATION}"
 
 cd /home/worker/landingzone
 for i in ${symbols[@]}; do
-        wget "http://ichart.finance.yahoo.com/table.csv?s=${i}&c=1962" -P $ROOT_LOCATION/historicalstocks/landingzone
+        wget "http://ichart.finance.yahoo.com/table.csv?s=${i}&c=1962" -O ${ROOT_LOCATION}/historicalstocks/landingzone/${i}.csv
 done
 
 hadoop fs -copyFromLocal ../historicalstocks/landingzone /user/worker/warehouse/stockprocessor/historic/input/
