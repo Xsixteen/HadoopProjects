039e9d148431a8ffd07adfc0703c6c710bb1f66d
Eric <x16.tux@gmail.com>
2017-02-12 15:23:39 -0500

    fixes

--- install.sh
+++ install.sh
@@ -2,9 +2,14 @@
 
 echo "HadoopScripts Project Installer===="
 
-echo "Please Specify Install Location: " 
+echo "Please Specify Install Location.  Make sure to use the full path, reference path below"
+pwd
+
+echo "Installation Path: " 
 
 read install_loc
 
 cp ./build/libs/* $install_loc/lib/
 cp ./scripts/* $install_loc/scripts/
+
+echo "Installation Completed===="
